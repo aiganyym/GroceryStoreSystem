f5060ac4f5e52b0fa9be47103640752b5248ad14
aiganyym <253601@astanait.edu.kz>
2025-12-27 08:32:01 +0500

    ..

--- Main.java
+++ Main.java
@@ -3,24 +3,24 @@ public class Main {
 
         System.out.println("=== Grocery Store System ===\n");
 
-        // products
+        
         Product apple = new Product(1, "Apple", 250, 30);
         Product milk = new Product(2, "Milk", 450, 10);
 
-        // customer
+        
         Customer customer = new Customer(101, "Student", "Standard", 0);
 
-        // show products
+        
         System.out.println("Products:");
         System.out.println(apple);
         System.out.println(milk);
 
-        // simulate purchase
+        
         double total = apple.getPrice() * 2;
         apple.reduceQuantity(2);
         customer.addPurchase(total);
 
-        // sale
+        
         Sale sale = new Sale(5001, customer.getName(), total, "Pending");
         sale.completeSale();
 
